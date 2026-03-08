bc73179f96c4a8637ad12802032808604a3446dc
Hridi12345 <hridimayesha13@gmail.com>
2026-03-08 12:11:49 +0600

    moved the basic js task from the assessment folder to lab folder

---
+++ lab/script.js
@@ -0,0 +1,26 @@
+let a=5;
+let b=10;
+[a,b]=[b,a];
+console.log(a,b);
+
+
+function square(n) {
+    return n * n;
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(square(i));
+}
+
+
+let numbers = [10, 25, 5, 40, 15];
+
+let largest = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+        largest = numbers[i];
+    }
+}
+
+console.log(largest);
